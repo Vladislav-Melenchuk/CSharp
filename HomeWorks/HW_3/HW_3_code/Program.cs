28f5f300abd59916764efba763d305bf17e9fe05
Vladislav <10328026@stud.op.edu.ua>
2025-01-13 14:15:15 +0200

    add HW_3

---
+++ HomeWorks/HW_3/HW_3_code/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Properties
+{
+    static class Input
+    {
+        static int number;
+
+        public static int Number()
+        {
+            while (true)
+            {
+                try
+                {
+                    number = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Нужно было ввести число, а не строку!");
+                    continue;
+                }
+                catch (System.OverflowException)
+                {
+                    Console.WriteLine("Слишком большое число!");
+                    continue;
+                }
+                break;
+            }
+            return number;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число: ");
+            int x = Input.Number();
+
+            for (int i = 1; i <= x; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write("  ");
+                }
+                Console.WriteLine("*");
+
+               
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write("  ");
+                }
+            }
+        }
+    }
+}
